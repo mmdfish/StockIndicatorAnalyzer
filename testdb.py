cdc59f13d7fc1d005c12632e7b85d2a136cb4403
mmdfish <77087140@qq.com>
2020-09-15 22:48:43 +0800

    add test

--- testdb.py
+++ testdb.py
@@ -3,6 +3,7 @@ import pandas as pd
 from datetime import datetime
 from sqlalchemy import create_engine
 import common
+import json
 
 def testAllStock():
     sql_cmd = "SELECT * FROM allstock"
@@ -133,10 +134,79 @@ def testStockQualification2():
     result = pd.read_sql(sql=sql_cmd, con=cx)
     print(result)
 
+def testStockQualification3():
+    cx = create_engine(common.db_path_sqlalchemy)
+
+    sql_cmd = "SELECT count(*) FROM stock_qualification where ma5_5 = '2' and date=(select max(date) from stock_qualification) and (code like 'sh.6%' or code like 'sz.00%' or code like 'sz.300%')"
+    cursor = cx.execute(sql_cmd)
+    result = cursor.fetchone()
+    totalNum = result[0]
+
+    sql_cmd = "SELECT * FROM stock_qualification where ma5_5 = '2' and date=(select max(date) from stock_qualification) and (code like 'sh.6%' or code like 'sz.00%' or code like 'sz.300%') limit 0,10"
+    #sql_cmd = "SELECT * FROM stock_spec where date=(select max(date) from stock_spec) order by amplitude_10 asc limit 0,50"
+    
+    result = pd.read_sql(sql=sql_cmd, con=cx)
+    df_json = result.to_json(orient = 'table', force_ascii = False)
+    data = json.loads(df_json)
+    data["total"] = totalNum
+    print(data)
+
+def testStockQualification4():
+    cx = create_engine(common.db_path_sqlalchemy)
+
+    sql_cmd = "SELECT * FROM stock_qualification where date=(select max(date) from stock_qualification) and (code like 'sh.6%' or code like 'sz.00%' or code like 'sz.300%') and dayk_desc_3 = 1 order by code asc limit 600,699"
+    result = pd.read_sql(sql=sql_cmd, con=cx)
+    print(result)
+
+    sql_cmd = "SELECT * FROM stock_qualification where date=(select max(date) from stock_qualification) and (code like 'sh.6%' or code like 'sz.00%' or code like 'sz.300%') and dayk_desc_3 = 1 order by code asc limit 700,799"
+    result = pd.read_sql(sql=sql_cmd, con=cx)
+    print(result)
+
+def testStockQualification5():
+    cx = create_engine(common.db_path_sqlalchemy)
+
+    # sql_cmd = "select * from stock_qualification where date=(select max(date) from stock_qualification)"
+    # result = pd.read_sql(sql=sql_cmd, con=cx)
+    # print(result)
+    sql_cmd = "select DISTINCT date from stock_qualification order by date desc limit 0,4"
+    cursor = cx.execute(sql_cmd)
+    result = cursor.fetchall()
+    date1 = result[0][0]
+    date2 = result[3][0]
+    print(date1, date2)
+    # date2 = cursor.fetchone()
+    # date3 = cursor.fetchone()
+    # date4 = cursor.fetchone()
+    
+    # sql_cmd = "SELECT * FROM stock_qualification where (dayk_desc_3 = '1' and date='" + date1 +"')"
+    #sql_cmd = "SELECT * FROM stock_qualification where (dayk_desc_3 = '1' and date='" + date1 +"') and \
+    #(ma5_10 = '1' and (date>='" + date2 + "' and date<='" + date1 + "')) and (code like 'sh.6%' or code like 'sz.00%' or code like 'sz.300%')"
+    sql_cmd = "SELECT * FROM stock_qualification where \
+    (ma5_10 = '1' and (date>='" + date2 + "' and date<='" + date1 + "')) and (code like 'sh.6%' or code like 'sz.00%' or code like 'sz.300%')"
+
+    result = pd.read_sql(sql=sql_cmd, con=cx)
+    print(result)
+
+def testStockQualification6():
+    cx = create_engine(common.db_path_sqlalchemy)
+    sql_cmd = "select DISTINCT date from stock_qualification order by date desc limit 0,5"
+    cursor = cx.execute(sql_cmd)
+    result = cursor.fetchall()
+    date1 = result[0][0]
+    date2 = result[4][0]
+    print(date1, date2)
+    
+    sql_cmd = "SELECT * FROM stock_qualification where (dayk_desc_3 = '1' or cross_up_ma10 = '1') and date='" + date1 +"' and code in (SELECT code FROM stock_qualification where \
+        (ma5_10 = '1' and (date>='" + date2 + "' and date<='" + date1 + "')) and (code like 'sh.6%' or code like 'sz.00%' or code like 'sz.300%'))"
+
+    result = pd.read_sql(sql=sql_cmd, con=cx)
+    print(result)
+
+
 def testDayK22():
     ticker = 'sh.000001'
-    sql_cmd = "SELECT * FROM stock_day_k where code='" + ticker + "'"
-    #sql_cmd = "SELECT * FROM stock_day_k where code='sh.000001' order by date asc limit 0,10"
+    #sql_cmd = "SELECT * FROM stock_day_k where code='" + ticker + "'"
+    sql_cmd = "SELECT * FROM stock_day_k where code='sh.000001' order by date asc limit 0,10"
     cx = create_engine(common.db_path_sqlalchemy)
     result = pd.read_sql(sql=sql_cmd, con=cx)
     print(result)
@@ -171,6 +241,11 @@ def testGetFactor():
     print(data_list)
     bs.logout()
 
+def testNumber() :
+    pageIndex = 0
+    startIndex = pageIndex * 100
+    stopIndex = pageIndex * 100 + 99
+    print(startIndex, stopIndex)
 #testAllStock()
 #testStockStatus()
 #testDayK()
@@ -182,6 +257,12 @@ def testGetFactor():
 #testSearchTime()
 #testStockhs300Spec()
 #testStockQualification()
-testStockQualification2()
+#testStockQualification2()
+#testStockQualification3()
+#testStockQualification4()
+#testStockQualification5()
+testStockQualification6()
 #testFactor()
 #testGetFactor()
+#testNumber()
+
